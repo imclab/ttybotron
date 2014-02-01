d499a26565cd9c0a9cfcb272880366a3cbce1df0
Andrew Mundy <andrew.mundy@ieee.org>
2014-02-01 17:05:27 +0000

    Initial urwid attempt.

---
+++ asciibotron.py
@@ -0,0 +1,39 @@
+import ttybotron, urwid
+
+class Asciibotron( object ):
+    def __init__( self ):
+        # Generate UI elements
+        #self.wdg_cols_top = urwid.Columns([])
+        self.wdg_list_msg = urwid.ListBox([])
+        #self.wdg_frame = urwid.Frame(
+        #    header = self.wdg_cols_top,
+        #    body = self.wdg_list_msg
+        #)
+
+        # Set up the message listening
+        self.socket = ttybotron.make_socket( 17892 )
+        self.recvr = ttybotron.SDPPrintReceiver( self.socket, self.receive )
+        self.messages = []
+    
+    def __call__( self ):
+        self.loop = urwid.MainLoop(
+        #    self.wdg_frame
+            self.wdg_list_msg,
+            unhandled_input = self.unhandled
+        )
+        self.recvr.start( )
+        self.loop.run( )
+
+    def receive( self, packet, addr ):
+        """Receive a new message."""
+        pass
+
+    def unhandled( self, input ):
+        if input in ['q', 'Q']:
+            self.recvr.exit = True
+            self.recvr.join()
+            raise urwid.ExitMainLoop
+
+if __name__ == "__main__":
+    tron = Asciibotron( )
+    tron()
