08e34cf1f9dd8f63cd0f35e05f8f79d299edac38
Andrew Mundy <andrew.mundy@ieee.org>
2014-02-03 11:36:33 +0000

    Modified headers, started fleshing out list box.

--- asciibotron.py
+++ asciibotron.py
@@ -1,4 +1,10 @@
-import ttybotron, urwid
+import sys, ttybotron
+
+try:
+    import urwid
+except:
+    sys.stderr.write( "Failed to import urwid, you must install urwid.\n" )
+    sys.exit( -1 )
 
 class AsbListBoxWalker( object ):
     pass
@@ -9,27 +15,65 @@ class AsbListBox( urwid.ListBox ):
         super( AsbListBox, self ).__init__( body )
 
     def add_message( self, packet, addr ):
-        # Get the current position
+        lbl_host = ""
+        lbl_x = urwid.Text( "%d" % packet.chip_x, align="right" )
+        lbl_y = urwid.Text( "%d" % packet.chip_y, align="right" )
+        lbl_c = urwid.Text( "%d" % packet.core, align="right" )
+        lbl_message = urwid.Text( packet.data )
+
+        clms = urwid.Columns([
+            (10, lbl_host),
+            ( 3, lbl_x),
+            ( 3, lbl_y),
+            ( 3, lbl_c),
+            lbl_message
+        ])
+
+        self.append( clms )
 
 class Asciibotron( object ):
     def __init__( self ):
-        # Generate UI elements
-        #self.wdg_cols_top = urwid.Columns([])
-        self.wdg_list_msg = urwid.ListBox([])
-        #self.wdg_frame = urwid.Frame(
-        #    header = self.wdg_cols_top,
-        #    body = self.wdg_list_msg
-        #)
+        ## Generate UI elements
+        self._palette = [
+            ('header', 'white, bold', 'light blue'),
+        ]
 
-        # Set up the message listening
+        # Labels for the columns
+        self._lbl_host = urwid.Text( "Host" )
+        self._lbl_x = urwid.Text( "X", align="right" )
+        self._lbl_y = urwid.Text( "Y", align="right" )
+        self._lbl_c = urwid.Text( "C", align="right" )
+        self._lbl_message = urwid.Text( "Message" )
+
+        # Header for the table
+        self.wdg_cols_top = urwid.Columns([
+            (10, self._lbl_host ),
+            ( 3, self._lbl_x ),
+            ( 3, self._lbl_y ),
+            ( 2, self._lbl_c ),
+            self._lbl_message,
+        ], dividechars = 1)
+        map1 = urwid.AttrMap( self.wdg_cols_top, 'header' )
+
+        # List box
+        self.wdg_list_msg = AsbListBox()
+
+        # Frame
+        self.wdg_frame = urwid.Frame(
+            header = map1,
+            body = self.wdg_list_msg
+        )
+
+        ## Set up the message listening
         self.socket = ttybotron.make_socket( 17892 )
-        self.recvr = ttybotron.SDPPrintReceiver( self.socket, self.receive )
+        self.recvr = ttybotron.SDPPrintReceiver( self.socket,
+            self.wdg_list_msg.add_message )
         self.messages = []
     
     def __call__( self ):
         self.loop = urwid.MainLoop(
-        #    self.wdg_frame
-            self.wdg_list_msg,
+            self.wdg_frame,
+            self._palette,
             unhandled_input = self.unhandled
         )
         self.recvr.start( )
